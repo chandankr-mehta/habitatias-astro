2f504861262c266fc833ba8e4c12905072350aae
Chandan Kumar <chandankumarmehta60@gmail.com>
2026-08-15 01:52:14 +0530

    Fix Astro server deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  output: "server",
+
   vite: {
     build: {
       sourcemap: false
